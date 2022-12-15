a25df2a7c0f5eb8e600974af021b2a1759bde07a
Hoang Tran <jashoang2583@gmail.com>
2022-12-14 20:28:49 -0800

    commit tree ac1ef84ad5c6644efe4f26a0ffc2cb9afefea7e9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
   # write code here
+  attr_accessor :kind
+
+
+  def initialize kind 
+    @kind = kind
+  end
+
+  def kind sides 
+    case sides
+    when sides == self.kind
+      return equilateral
+    end
+
+  end
 end
